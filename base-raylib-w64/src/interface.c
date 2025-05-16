7e60c4f5969e6b2dc77d6a4fe051ad318f66101c
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-05-16 18:52:27 -0300

    Update interface.c

--- base-raylib-w64/src/interface.c
+++ base-raylib-w64/src/interface.c
@@ -1,18 +1,17 @@
 #include "interface.h"
 #include "raylib.h"
-#include "ia.h"
-#include "pistas.h"
 #include <string.h>
+
 
 #define SCREEN_WIDTH 1800
 #define SCREEN_HEIGHT 950
 
-void mostrarMenuPrincipal();
 
 void mostrarIntroducao() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Quem é o Crush Secreto da Barbie?");
     InitAudioDevice();
     SetTargetFPS(60);
+
 
     Texture2D fundo = LoadTexture("assets/casa.jpg");
     Texture2D carro = LoadTexture("assets/carro.png");
@@ -21,12 +20,15 @@ void mostrarIntroducao() {
     Music menuMusica = LoadMusicStream("assets/DreamHouse.mp3");
     PlayMusicStream(introMusica);
 
+
     float posCarroX = SCREEN_WIDTH + (carro.width * 1.8f);
     float posCarroY = 150;
     float posBarbieX = -barbie.width * 2.0f;
 
+
     bool carroSaiu = false;
     bool barbieChegou = false;
+
 
     const char *frases[] = {
         "Olá! Eu sou a Barbie... e estou prestes a viver um mistério cheio de romance!",
@@ -43,9 +45,11 @@ void mostrarIntroducao() {
     bool fadingOutIntro = false;
     bool fadingInMenu = false;
 
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
 
         if (!pulouIntro) {
             UpdateMusicStream(introMusica);
@@ -61,20 +65,23 @@ void mostrarIntroducao() {
                 UpdateMusicStream(menuMusica);
                 if (fadingInMenu && fade < 1.0f) {
                     fade += 0.01f;
-                    SetMusicVolume(menuMusica, 0.3f * fade);
+                    SetMusicVolume(menuMusica, 0.3f * fade); // volume mais baixo no máximo
                 }
             }
 
+
             if (fadingOutIntro && fade < 1.0f) {
-                fade += 0.005f;
+                fade += 0.005f; // fade out mais lento da intro
                 SetMusicVolume(introMusica, 1.0f - fade);
                 if (fade >= 1.0f) StopMusicStream(introMusica);
             }
         }
 
+
         if (!pulouIntro) {
             DrawTexturePro(fundo, (Rectangle){0, 0, fundo.width, fundo.height},
                            (Rectangle){0, 0, SCREEN_WIDTH, SCREEN_HEIGHT}, (Vector2){0, 0}, 0.0f, WHITE);
+
 
             if (!carroSaiu) {
                 DrawTexturePro(carro, (Rectangle){0, 0, carro.width, carro.height},
@@ -83,13 +90,16 @@ void mostrarIntroducao() {
                 posCarroX -= 6.5f;
                 posCarroY += 1.2f;
 
+
                 if (posCarroX < -(carro.width * 1.8f)) carroSaiu = true;
             }
+
 
             if (carroSaiu) {
                 DrawTexturePro(barbie, (Rectangle){0, 0, barbie.width, barbie.height},
                                (Rectangle){posBarbieX, 350, barbie.width * 2.8f, barbie.height * 2.8f},
                                (Vector2){0, 0}, 0.0f, WHITE);
+
 
                 if (posBarbieX < SCREEN_WIDTH * -0.25f) {
                     posBarbieX += 4.5f;
@@ -100,6 +110,7 @@ void mostrarIntroducao() {
                 }
             }
 
+
             if (mostrarTexto) {
                 tempoTexto += GetFrameTime();
                 if (tempoTexto > 0.05f && textoIndex < strlen(frases[fraseAtual])) {
@@ -107,8 +118,10 @@ void mostrarIntroducao() {
                     tempoTexto = 0;
                 }
 
+
                 DrawRectangle(800, 820, 880, 60, (Color){255, 192, 203, 180});
                 DrawText(TextSubtext(frases[fraseAtual], 0, textoIndex), 820, 840, 24, BLACK);
+
 
                 DrawText("Pressione ENTER para continuar", 600, 900, 20, DARKGRAY);
                 if (IsKeyPressed(KEY_ENTER) && textoIndex >= strlen(frases[fraseAtual])) {
@@ -118,11 +131,10 @@ void mostrarIntroducao() {
                 }
             }
 
+
             DrawText("Pressione ESPAÇO para pular", 20, 900, 20, DARKGRAY);
             if (IsKeyPressed(KEY_SPACE)) {
-                CloseWindow();
-                mostrarMenuPrincipal();
-                break;
+                pulouIntro = true;
             }
         } else {
             ClearBackground((Color){255, 182, 193, 255});
@@ -131,15 +143,16 @@ void mostrarIntroducao() {
             DrawText("Use as setas para se mover, pressione 1, 2 ou 3 para responder perguntas.", 280, 360, 26, BLACK);
             DrawText("Pressione ESPAÇO para Começar", 600, 500, 28, DARKGRAY);
 
+
             if (IsKeyPressed(KEY_SPACE)) {
-                CloseWindow();
-                mostrarMenuPrincipal();
                 break;
             }
         }
 
+
         EndDrawing();
     }
+
 
     UnloadTexture(fundo);
     UnloadTexture(carro);
@@ -147,44 +160,5 @@ void mostrarIntroducao() {
     UnloadMusicStream(introMusica);
     UnloadMusicStream(menuMusica);
     CloseAudioDevice();
-    CloseWindow();
-}
-
-void mostrarMenuPrincipal() {
-    Pista* listaDePistas = NULL;
-
-    InitWindow(800, 600, "Barbie's Love Detective - Menu");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground((Color){255, 228, 225, 255});
-
-        DrawText("MENU PRINCIPAL", 280, 40, 30, MAGENTA);
-        DrawText("1 - Cena: Mensagem Misteriosa", 80, 120, 20, BLACK);
-        DrawText("2 - Cena: Brownie na Cantina", 80, 160, 20, BLACK);
-        DrawText("3 - Ver pistas coletadas", 80, 200, 20, BLACK);
-        DrawText("4 - Sair do jogo", 80, 240, 20, DARKGRAY);
-
-        EndDrawing();
-
-        if (IsKeyPressed(KEY_ONE)) {
-            cenaMensagemMisteriosa(&listaDePistas, 1);
-        }
-        if (IsKeyPressed(KEY_TWO)) {
-            cenaBrownieCantina(&listaDePistas, 1);
-        }
-        if (IsKeyPressed(KEY_THREE)) {
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("PISTAS COLETADAS:", 20, 20, 20, DARKBLUE);
-            mostrarPistasRaylib(listaDePistas);
-            EndDrawing();
-            while (!IsKeyPressed(KEY_ENTER)) {} // espera ENTER
-        }
-        if (IsKeyPressed(KEY_FOUR)) break;
-    }
-
-    liberarPistas(listaDePistas);
     CloseWindow();
 }
